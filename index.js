ca39c32cdb067ec3d1fde17bfa15b7b6e92b14e8
Mariya Batanova <batanova708@gmail.com>
2023-01-17 01:18:22 +0200

    method indexOf

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//Mariaa Batanova
+//Написати функцію, яка приймає масив чисел
+//і повертає true, якщо в цьому масиві є два однакових числа поспіль
+//Якщо не має - повертає false, якщо масив порожній - теж повертає false
+
 const arr1 = [1,2,3,4]; //false
 const arr2 = [1,2,2]; //true
 /**
@@ -17,3 +22,20 @@ function getTwoSameNumbers(arr){
 
 console.log(getTwoSameNumbers(arr1));
 console.log(getTwoSameNumbers(arr2));
+
+/**
+ * 
+ * @param {number} arr 
+ * @returns {boolean}
+ */
+function getTwoNumIndexOf(arr) {
+  if (arr.length === 0) return false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i], i + 1) === i + 1) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(getTwoNumIndexOf(arr1));
+console.log(getTwoNumIndexOf(arr2));
